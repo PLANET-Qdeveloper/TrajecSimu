61fc2628d9c44399bddaa4ee611568d216266e72
misohiyoko <misohiyoko@gmail.com>
2025-05-13 17:46:19 +0900

    Refactor JSBSim execution to synchronous model,

--- src/jsbsim/jsb_runner.py
+++ src/jsbsim/jsb_runner.py
@@ -1,22 +1,9 @@
-import asyncio
-
 import jsbsim
 
+fdm = jsbsim.FGFDMExec(r"./src/jsbsim/param-xml")  # Use JSBSim default aircraft data.
+fdm.load_script("pq_simulation.xml")
 
-async def run_jsbsim():
-    fdm = jsbsim.FGFDMExec(r"./src/jsbsim/param-xml")  # Use JSBSim default aircraft data.
-    fdm.load_script("pq_simulation.xml")
-    fdm.run_ic()
+fdm.run_ic()
 
-    while fdm.run():
-        pass
-
-
-async def main():
-    await asyncio.gather(
-        run_jsbsim(),
-    )
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+while fdm.run():
+    pass
